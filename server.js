537697b1138507bcbe71f367035481a6e495f662
macbook-jack <jik.jack@gmail.com>
2015-03-31 18:13:58 -0700

    emailing works now

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var PORT = 8080;
 // Express is a web framework for node.js
 // that makes nontrivial applications easier to build
 var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
 
 // Create the server instance
 var app = express();
@@ -10,6 +12,7 @@ var app = express();
 // Print logs to the console and compress pages we send
 app.use(express.logger());
 app.use(express.compress());
+app.use(bodyParser());
 
 // Return all pages in the /static directory
 // whenever they are requested at '/'
@@ -21,4 +24,25 @@ app.use(express.static(__dirname + '/static'));
 var port = process.env.PORT || PORT; // 80 for web, 3000 for development
 app.listen(port, function() {
 	console.log("Node.js server running on port %s", port);
+});
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.post('/sendemail',function(req,res){
+    var transporter = nodemailer.createTransport();
+	
+	console.log(req.body.email);
+	console.log(req.body.name);
+	console.log(req.body.message);
+
+	transporter.sendMail({
+	    from: req.body.email,
+	    to: 'jik.jack@gmail.com',
+	    subject: 'Hello from ' + req.body.name,
+	    text: req.body.message
+	});
+
+    res.redirect("/");
 });
